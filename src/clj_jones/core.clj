a2a8e240102ac790df6d3667ef5056218f218d05
George Courtsunis <gjcourt@gmail.com>
2012-09-17 17:02:11 -0700

    write macro to handle watches

--- src/clj_jones/core.clj
+++ src/clj_jones/core.clj
@@ -1,5 +1,6 @@
 (ns clj-jones.core
   (:import [com.netflix.curator.framework CuratorFrameworkFactory])
+  (:import [com.netflix.curator.framework.api CuratorWatcher])
   (:import [com.netflix.curator.retry RetryUntilElapsed])
   (:require [clojure.contrib.io :as io])
   (:require [clojure.contrib.string :as string])
@@ -10,7 +11,7 @@
   [string]
   (io/to-byte-array string))
 
-(defn client
+(defn mk-jones-client
   [hosts ports service]
   (let [hostports (string/join "," (map #(string/join ":" %&)
                                         hosts
@@ -22,23 +23,45 @@
      :service service}
     ))
 
+(defmacro mk-handler
+  [& body]
+  `(reify CuratorWatcher
+     (process [this# event#]
+       (let [~'event {:state (.getState event#)
+                      :path (.getPath event#)
+                      :type (.getType event#)}]
+         ~@body
+         ))))
+
 (defn mk-path
   [service]
   (str "/services/" service "/conf"))
 
 (defn- get-hmap
-  [client]
-  (let [builder (.getData (:zk client))
-        path (mk-path (:service client))]
-    (json/decode
-      (apply str (map char (-> builder (.forPath path)))))
-    ))
+  ([client]
+    (let [builder (.getData (:zk client))
+          path (mk-path (:service client))]
+      (json/decode
+        (apply str (map char (-> builder
+                               (.forPath path))))
+        )))
+  ([client handler]
+    (let [builder (.getData (:zk client))
+          path (mk-path (:service client))]
+      (json/decode
+        (apply str (map char (-> builder
+                               (.usingWatcher handler)
+                               (.forPath path)))))
+      )))
 
 (defn get
-  [client key]
-  (let [hmap (get-hmap client)]
-    (hmap key)
-    ))
+  ([client key]
+   (let [hmap (get-hmap client)]
+     (hmap key)))
+  ([client key handler]
+   (let [hmap (get-hmap client handler)]
+     (hmap key)
+     )))
 
 (defn set
   [client key value]
@@ -57,6 +80,7 @@
         builder (.create (:zk client))
         path (mk-path (:service client))]
     (-> builder
+      ; (.usingWatcher (WatchHandler.))
       (.creatingParentsIfNeeded
         (.forPath path
                   (utf8-byte-array
@@ -73,3 +97,6 @@
                 (utf8-byte-array
                   (json/encode (dissoc hmap key)))))
     ))
+
+(def jones
+  (mk-jones-client ["localhost"] [2181] "storm"))
