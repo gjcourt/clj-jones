5fc22b018bb39e92ee70dab09e55568beabe26aa
George Courtsunis <gjcourt@gmail.com>
2012-11-08 16:16:43 -0800

    adjust zk deps

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject com.gjcourt/clj-jones "0.0.2"
+(defproject com.gjcourt/clj-jones "0.0.2-wip6"
   :main clj-jones.core
   :description "Clojure client for Jones"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [com.netflix.curator/curator-framework "1.1.18"]
-                 [com.netflix.curator/curator-recipes "1.1.18"]
+  :dependencies [[com.netflix.curator/curator-framework "1.0.1"]
+                 [com.netflix.curator/curator-recipes "1.0.1"]
                  [cheshire "4.0.2"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  ])
